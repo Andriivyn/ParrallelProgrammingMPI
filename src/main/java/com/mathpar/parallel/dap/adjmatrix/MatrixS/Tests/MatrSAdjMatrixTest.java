4ab2c9e431d2ca4b6badc3f9db59ca7601dbf5d4
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-12-03 16:29:49 +0200

    fixed MatrSAdjMatrixTest

--- src/main/java/com/mathpar/parallel/dap/adjmatrix/MatrixS/Tests/MatrSAdjMatrixTest.java
+++ src/main/java/com/mathpar/parallel/dap/adjmatrix/MatrixS/Tests/MatrSAdjMatrixTest.java
@@ -13,41 +13,65 @@ import org.javatuples.Pair;
 
 import java.io.IOException;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class MatrSAdjMatrixTest extends DAPTest {
     private final static MpiLogger LOGGER = MpiLogger.getLogger(MatrSMult4Test.class);
     protected MatrSAdjMatrixTest() {
         super("MatrSAdjMatrix", 7701, 0);
+        ring = new Ring("Z[]");
     }
 
     @Override
     protected Element[] initData(int size, int density, int maxBits, Ring ring) {
         MatrixS M = matrix(size, density, maxBits, ring);
-        return new Element[]{M, M.det(ring)};
+        return new Element[]{M, ring.numberONE};
     }
 
     @Override
     protected Pair<Boolean, Element> checkResult(DispThread dispThread, String[] args, Element[] initData, Element[] resultData, Ring ring) {
         AdjMatrixS resAdj = (AdjMatrixS) resultData[0];
+//        LOGGER.info("Res A = " + resAdj.A);
+//        LOGGER.info("Res det = " + resDet);
+//        LOGGER.info("Res S = " + resAdj.S);
         MatrixS initM = (MatrixS) initData[0];
-        Element d0 = initData[1];
-        AdjMatrixS toCompare = new AdjMatrixS(initM, d0, ring);
-        boolean succeed = resAdj.A.equals(toCompare.A);
-        return new Pair<>(succeed, resAdj.Det);
+        AdjMatrixS toCompare = new AdjMatrixS(initM, ring.numberONE, ring);
+//        LOGGER.info("toCompare A = " + toCompare.A);
+//        LOGGER.info("toCompare d = "+ toCompare.Det);
+//        LOGGER.info("toCompare S = "+ toCompare.S);
+        boolean succeed = resAdj.A.equals(toCompare.A, ring) && resAdj.S.equals(toCompare.S, ring) && resAdj.Det.equals(toCompare.Det);
+        return new Pair<>(succeed, null);
+    }
+
+    @Override
+    protected int dispRunsOnOtherProc() {
+        return 0;
     }
 
     @Override
     protected MatrixS matrix(int size, int density, int maxBits, Ring ring){
+//        int [][]mat = {{6, -5, 8, 4}, {9,7,5,2}, {7,5,3,7}, {-4,8,-8,-3}};
+//        MatrixS matrix = new MatrixS(mat, ring);
         MatrixS matrix = new MatrixS(size, size, density, new int[]{maxBits}, new Random(),ring.numberONE(), ring);
         // LOGGER.trace("bef matrix = " + matrix);
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if(!matrix.getElement(i,j, ring).equals(ring.numberZERO))
-                    matrix.putElement( ring.numberONE.divide(matrix.getElement(i,j, ring),  ring), i, j);
-            }
-        }
+//        for (int i = 0; i < size; i++) {
+//            for (int j = 0; j < size; j++) {
+//                if(!matrix.getElement(i,j, ring).equals(ring.numberZERO))
+//                    matrix.putElement( ring.numberONE.divide(matrix.getElement(i,j, ring),  ring), i, j);
+//            }
+//        }
         //LOGGER.info("matrix = " + matrix);
         return matrix;
+    }
+
+    @Override
+    protected Element[] sequentialExecute(Element[] data, Ring ring) {
+        MatrixS m = (MatrixS) data[0];
+        Element d0 = data[1];
+        AdjMatrixS adjM = new AdjMatrixS(m, d0,  ring);
+        Element resD = adjM.Det;
+        MatrixS y = adjM.S.ES_min_dI(resD, adjM.Ei, adjM.Ej, ring);
+        return new Element[] {adjM, y, resD};
     }
 
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException, MPIException, IOException {
